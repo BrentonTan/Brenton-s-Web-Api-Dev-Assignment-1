4e60e0c709224d0f8bba84597e0a292a57ce9c10
BrentonTan <brentontan123@gmail.com>
2025-11-19 12:23:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ hotel.createRoom(103, "Standard", 90.00, ["WiFi"], true);
 hotel.createRoom(104, "Executive", 180.00, ["WiFi", "TV", "Workspace"], true);
 hotel.createRoom(105, "Penthouse", 500.00, ["WiFi", "TV", "Jacuzzi", "Kitchen"], false);
 
-//Retrieve all room numbers
-const allRoomNos = hotel.retrieveRooms();
-console.log(allRoomNos);
+//Browse all rooms
+console.log(hotel.browseRooms());
 
 //Delete room 104
 const isDeleted = hotel.deleteRoom(104);
@@ -19,13 +18,19 @@ console.log(isDeleted ? "Room 104 deleted." : "Room 104 not found.");
 const updatedRoom = hotel.retrieveRooms();
 console.log(updatedRoom);
 
+//Check for deleted room
+console.log(hotel.browseRooms());
+
 //Get details of room by room number
-console.log(hotel.roomDetails(105));
+console.log(hotel.roomDetails(102));
 
 //If room is not available
 console.log(hotel.roomDetails(104));
-console.log(hotel.roomDetails(102));
 
 //Update room details
-hotel.amendRoom(102, { nightlyRate: 275, bookedStatus: true, amenities: ["WiFi", "TV", "Mini Bar"] });
+hotel.amendRoom(102, { nightlyRate: 275, amenities: ["WiFi", "TV", "Mini Bar"] });
 console.log("After update:", hotel.roomDetails(102));
+
+//Book a room
+hotel.bookRoom(105);
+console.log(hotel.roomDetails(105));
